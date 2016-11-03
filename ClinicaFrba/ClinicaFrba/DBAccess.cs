c1a80f80a59e3c965d9331537facfcaf6625bef2
evguenitessov <tessov.evgueni@hotmail.com>
2016-11-03 14:08:50 -0300

    - Se agrega el archivo de configuracion y se cambia la recuperacion del conection string la aplicacion.

--- ClinicaFrba/ClinicaFrba/DBAccess.cs
+++ ClinicaFrba/ClinicaFrba/DBAccess.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Data.SqlClient;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -13,8 +14,62 @@ namespace ClinicaFrba
         public string Conexion { get; set; }
 
         public DBAccess() 
+        {            
+            Conexion = @"" + cadenaConexion() + "";
+        }
+
+        static public string fechaSystem()
         {
-            Conexion = @"Data Source=ESTUDIO\SQLSERVER2012;Initial Catalog=GD2C2016;User ID=gd;Password=gd2016";
+            StreamReader config = new StreamReader("../../configuracion.txt");
+            string linea = "";
+            string buffer = config.ReadLine();
+            while (buffer != null)
+            {
+                if (buffer.Substring(0, 5) == "Fecha")
+                {
+                    linea = buffer;
+                }
+                buffer = config.ReadLine();
+            }
+            config.Close();
+
+            return (linea.Substring(13, 4) + "-" + linea.Substring(7, 2)) + "-" + linea.Substring(10, 2); ;
+
+        }
+
+        static public string cadenaConexion()
+        {
+            string user = "";
+            string pass = "";
+            string dtSrc = "";
+            string iniCtlg = "";
+            StreamReader config = new StreamReader("../../configuracion.txt");
+            string buffer = "";
+            buffer = config.ReadLine();
+            while (buffer != null)
+            {
+                if (buffer.Substring(0, 4) == "Data")
+                {
+                    dtSrc = buffer.Substring(13);
+                }
+
+                if (buffer.Substring(0, 4) == "Init")
+                {
+                    iniCtlg = buffer.Substring(17);
+                }
+
+                if (buffer.Substring(0, 4) == "User")
+                {
+                    user = buffer.Substring(9);
+                }
+                if (buffer.Substring(0, 4) == "Pass")
+                {
+                    pass = buffer.Substring(10);
+                }
+                buffer = config.ReadLine();
+            }
+            config.Close();
+            return "Data Source=" + dtSrc + ";Initial Catalog=" + iniCtlg + ";User ID=" + user + ";Password=" + pass;
         }
     }
 }
