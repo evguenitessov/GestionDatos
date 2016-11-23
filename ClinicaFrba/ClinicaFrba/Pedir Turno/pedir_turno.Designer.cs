01c0379e3ecc919a76c89c2fde7786f063c194ef
sora797 <natitorokian@gmail.com>
2016-11-23 15:46:19 -0300

    agrego codigo en pedir turno: seleccion de especialidades y profesional y modifico algo de cancelaciones

--- ClinicaFrba/ClinicaFrba/Pedir Turno/pedir_turno.Designer.cs
+++ ClinicaFrba/ClinicaFrba/Pedir Turno/pedir_turno.Designer.cs
@@ -29,35 +29,35 @@
         private void InitializeComponent()
         {
             this.box_especialidad = new System.Windows.Forms.GroupBox();
-            this.groupBox2 = new System.Windows.Forms.GroupBox();
-            this.groupBox3 = new System.Windows.Forms.GroupBox();
-            this.groupBox4 = new System.Windows.Forms.GroupBox();
-            this.groupBox5 = new System.Windows.Forms.GroupBox();
-            this.label1 = new System.Windows.Forms.Label();
+            this.combo_especialidades = new System.Windows.Forms.ComboBox();
             this.aceptar_especialidad = new System.Windows.Forms.Button();
-            this.cbox_especialidades = new System.Windows.Forms.ComboBox();
-            this.cbox_profesionales = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.profesional = new System.Windows.Forms.GroupBox();
+            this.combo_profesionales = new System.Windows.Forms.ComboBox();
             this.aceptar_profesional = new System.Windows.Forms.Button();
             this.label2 = new System.Windows.Forms.Label();
-            this.atras = new System.Windows.Forms.Button();
+            this.horario = new System.Windows.Forms.GroupBox();
             this.confirmar_turno = new System.Windows.Forms.Button();
-            this.grid_fechas = new System.Windows.Forms.DataGridView();
             this.grid_horarios = new System.Windows.Forms.DataGridView();
-            this.aceptar_fecha = new System.Windows.Forms.Button();
-            this.elegir_fecha = new System.Windows.Forms.DataGridViewCheckBoxColumn();
             this.elegir_horario = new System.Windows.Forms.DataGridViewCheckBoxColumn();
+            this.groupBox4 = new System.Windows.Forms.GroupBox();
+            this.atras = new System.Windows.Forms.Button();
+            this.fecha = new System.Windows.Forms.GroupBox();
+            this.aceptar_fecha = new System.Windows.Forms.Button();
+            this.grid_fechas = new System.Windows.Forms.DataGridView();
+            this.elegir_fecha = new System.Windows.Forms.DataGridViewCheckBoxColumn();
             this.box_especialidad.SuspendLayout();
-            this.groupBox2.SuspendLayout();
-            this.groupBox3.SuspendLayout();
-            this.groupBox4.SuspendLayout();
-            this.groupBox5.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.grid_fechas)).BeginInit();
+            this.profesional.SuspendLayout();
+            this.horario.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.grid_horarios)).BeginInit();
+            this.groupBox4.SuspendLayout();
+            this.fecha.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.grid_fechas)).BeginInit();
             this.SuspendLayout();
             // 
             // box_especialidad
             // 
-            this.box_especialidad.Controls.Add(this.cbox_especialidades);
+            this.box_especialidad.Controls.Add(this.combo_especialidades);
             this.box_especialidad.Controls.Add(this.aceptar_especialidad);
             this.box_especialidad.Controls.Add(this.label1);
             this.box_especialidad.Font = new System.Drawing.Font("Calibri", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
@@ -68,61 +68,14 @@
             this.box_especialidad.TabStop = false;
             this.box_especialidad.Text = "Seleccione especialidad";
             // 
-            // groupBox2
+            // combo_especialidades
             // 
-            this.groupBox2.Controls.Add(this.cbox_profesionales);
-            this.groupBox2.Controls.Add(this.aceptar_profesional);
-            this.groupBox2.Controls.Add(this.label2);
-            this.groupBox2.Font = new System.Drawing.Font("Calibri", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.groupBox2.Location = new System.Drawing.Point(12, 127);
-            this.groupBox2.Name = "groupBox2";
-            this.groupBox2.Size = new System.Drawing.Size(560, 122);
-            this.groupBox2.TabIndex = 1;
-            this.groupBox2.TabStop = false;
-            this.groupBox2.Text = "Seleccione profesional";
-            // 
-            // groupBox3
-            // 
-            this.groupBox3.Controls.Add(this.confirmar_turno);
-            this.groupBox3.Controls.Add(this.grid_horarios);
-            this.groupBox3.Font = new System.Drawing.Font("Calibri", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.groupBox3.Location = new System.Drawing.Point(294, 255);
-            this.groupBox3.Name = "groupBox3";
-            this.groupBox3.Size = new System.Drawing.Size(278, 249);
-            this.groupBox3.TabIndex = 1;
-            this.groupBox3.TabStop = false;
-            this.groupBox3.Text = "Seleccione UN horario";
-            // 
-            // groupBox4
-            // 
-            this.groupBox4.Controls.Add(this.atras);
-            this.groupBox4.Font = new System.Drawing.Font("Calibri", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.groupBox4.Location = new System.Drawing.Point(12, 506);
-            this.groupBox4.Name = "groupBox4";
-            this.groupBox4.Size = new System.Drawing.Size(560, 53);
-            this.groupBox4.TabIndex = 1;
-            this.groupBox4.TabStop = false;
-            // 
-            // groupBox5
-            // 
-            this.groupBox5.Controls.Add(this.aceptar_fecha);
-            this.groupBox5.Controls.Add(this.grid_fechas);
-            this.groupBox5.Font = new System.Drawing.Font("Calibri", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.groupBox5.Location = new System.Drawing.Point(12, 255);
-            this.groupBox5.Name = "groupBox5";
-            this.groupBox5.Size = new System.Drawing.Size(276, 249);
-            this.groupBox5.TabIndex = 1;
-            this.groupBox5.TabStop = false;
-            this.groupBox5.Text = "Seleccione UNA fecha";
-            // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(206, 22);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(147, 18);
-            this.label1.TabIndex = 0;
-            this.label1.Text = "Especialidades Médicas";
+            this.combo_especialidades.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.combo_especialidades.FormattingEnabled = true;
+            this.combo_especialidades.Location = new System.Drawing.Point(183, 43);
+            this.combo_especialidades.Name = "combo_especialidades";
+            this.combo_especialidades.Size = new System.Drawing.Size(192, 26);
+            this.combo_especialidades.TabIndex = 2;
             // 
             // aceptar_especialidad
             // 
@@ -133,23 +86,36 @@
             this.aceptar_especialidad.Text = "Aceptar";
             this.aceptar_especialidad.UseVisualStyleBackColor = true;
             // 
-            // cbox_especialidades
+            // label1
             // 
-            this.cbox_especialidades.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
-            this.cbox_especialidades.FormattingEnabled = true;
-            this.cbox_especialidades.Location = new System.Drawing.Point(183, 43);
-            this.cbox_especialidades.Name = "cbox_especialidades";
-            this.cbox_especialidades.Size = new System.Drawing.Size(192, 26);
-            this.cbox_especialidades.TabIndex = 2;
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(206, 22);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(153, 18);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Especialidades Médicas";
             // 
-            // cbox_profesionales
+            // profesional
             // 
-            this.cbox_profesionales.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
-            this.cbox_profesionales.FormattingEnabled = true;
-            this.cbox_profesionales.Location = new System.Drawing.Point(183, 43);
-            this.cbox_profesionales.Name = "cbox_profesionales";
-            this.cbox_profesionales.Size = new System.Drawing.Size(192, 26);
-            this.cbox_profesionales.TabIndex = 5;
+            this.profesional.Controls.Add(this.combo_profesionales);
+            this.profesional.Controls.Add(this.aceptar_profesional);
+            this.profesional.Controls.Add(this.label2);
+            this.profesional.Font = new System.Drawing.Font("Calibri", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.profesional.Location = new System.Drawing.Point(12, 127);
+            this.profesional.Name = "profesional";
+            this.profesional.Size = new System.Drawing.Size(560, 122);
+            this.profesional.TabIndex = 1;
+            this.profesional.TabStop = false;
+            this.profesional.Text = "Seleccione profesional";
+            // 
+            // combo_profesionales
+            // 
+            this.combo_profesionales.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.combo_profesionales.FormattingEnabled = true;
+            this.combo_profesionales.Location = new System.Drawing.Point(183, 43);
+            this.combo_profesionales.Name = "combo_profesionales";
+            this.combo_profesionales.Size = new System.Drawing.Size(192, 26);
+            this.combo_profesionales.TabIndex = 5;
             // 
             // aceptar_profesional
             // 
@@ -165,18 +131,21 @@
             this.label2.AutoSize = true;
             this.label2.Location = new System.Drawing.Point(233, 22);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(89, 18);
+            this.label2.Size = new System.Drawing.Size(93, 18);
             this.label2.TabIndex = 3;
             this.label2.Text = "Profesionales";
             // 
-            // atras
+            // horario
             // 
-            this.atras.Location = new System.Drawing.Point(183, 15);
-            this.atras.Name = "atras";
-            this.atras.Size = new System.Drawing.Size(192, 32);
-            this.atras.TabIndex = 6;
-            this.atras.Text = "Atrás";
-            this.atras.UseVisualStyleBackColor = true;
+            this.horario.Controls.Add(this.confirmar_turno);
+            this.horario.Controls.Add(this.grid_horarios);
+            this.horario.Font = new System.Drawing.Font("Calibri", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.horario.Location = new System.Drawing.Point(294, 255);
+            this.horario.Name = "horario";
+            this.horario.Size = new System.Drawing.Size(278, 249);
+            this.horario.TabIndex = 1;
+            this.horario.TabStop = false;
+            this.horario.Text = "Seleccione UN horario";
             // 
             // confirmar_turno
             // 
@@ -186,16 +155,6 @@
             this.confirmar_turno.TabIndex = 7;
             this.confirmar_turno.Text = "Confirmar Turno";
             this.confirmar_turno.UseVisualStyleBackColor = true;
-            // 
-            // grid_fechas
-            // 
-            this.grid_fechas.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.grid_fechas.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
-            this.elegir_fecha});
-            this.grid_fechas.Location = new System.Drawing.Point(6, 25);
-            this.grid_fechas.Name = "grid_fechas";
-            this.grid_fechas.Size = new System.Drawing.Size(259, 178);
-            this.grid_fechas.TabIndex = 2;
             // 
             // grid_horarios
             // 
@@ -207,6 +166,42 @@
             this.grid_horarios.Size = new System.Drawing.Size(259, 178);
             this.grid_horarios.TabIndex = 3;
             // 
+            // elegir_horario
+            // 
+            this.elegir_horario.HeaderText = "Seleccionar";
+            this.elegir_horario.Name = "elegir_horario";
+            // 
+            // groupBox4
+            // 
+            this.groupBox4.Controls.Add(this.atras);
+            this.groupBox4.Font = new System.Drawing.Font("Calibri", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox4.Location = new System.Drawing.Point(12, 506);
+            this.groupBox4.Name = "groupBox4";
+            this.groupBox4.Size = new System.Drawing.Size(560, 53);
+            this.groupBox4.TabIndex = 1;
+            this.groupBox4.TabStop = false;
+            // 
+            // atras
+            // 
+            this.atras.Location = new System.Drawing.Point(183, 15);
+            this.atras.Name = "atras";
+            this.atras.Size = new System.Drawing.Size(192, 32);
+            this.atras.TabIndex = 6;
+            this.atras.Text = "Atrás";
+            this.atras.UseVisualStyleBackColor = true;
+            // 
+            // fecha
+            // 
+            this.fecha.Controls.Add(this.aceptar_fecha);
+            this.fecha.Controls.Add(this.grid_fechas);
+            this.fecha.Font = new System.Drawing.Font("Calibri", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.fecha.Location = new System.Drawing.Point(12, 255);
+            this.fecha.Name = "fecha";
+            this.fecha.Size = new System.Drawing.Size(276, 249);
+            this.fecha.TabIndex = 1;
+            this.fecha.TabStop = false;
+            this.fecha.Text = "Seleccione UNA fecha";
+            // 
             // aceptar_fecha
             // 
             this.aceptar_fecha.Location = new System.Drawing.Point(42, 209);
@@ -216,37 +211,42 @@
             this.aceptar_fecha.Text = "Aceptar";
             this.aceptar_fecha.UseVisualStyleBackColor = true;
             // 
+            // grid_fechas
+            // 
+            this.grid_fechas.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.grid_fechas.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.elegir_fecha});
+            this.grid_fechas.Location = new System.Drawing.Point(6, 25);
+            this.grid_fechas.Name = "grid_fechas";
+            this.grid_fechas.Size = new System.Drawing.Size(259, 178);
+            this.grid_fechas.TabIndex = 2;
+            // 
             // elegir_fecha
             // 
             this.elegir_fecha.HeaderText = "Seleccionar";
             this.elegir_fecha.Name = "elegir_fecha";
-            // 
-            // elegir_horario
-            // 
-            this.elegir_horario.HeaderText = "Seleccionar";
-            this.elegir_horario.Name = "elegir_horario";
             // 
             // pedir_turno
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(584, 561);
-            this.Controls.Add(this.groupBox2);
-            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.profesional);
+            this.Controls.Add(this.horario);
             this.Controls.Add(this.groupBox4);
-            this.Controls.Add(this.groupBox5);
+            this.Controls.Add(this.fecha);
             this.Controls.Add(this.box_especialidad);
             this.Name = "pedir_turno";
             this.Text = "Clinica FRBA - Solicitud de turno";
             this.box_especialidad.ResumeLayout(false);
             this.box_especialidad.PerformLayout();
-            this.groupBox2.ResumeLayout(false);
-            this.groupBox2.PerformLayout();
-            this.groupBox3.ResumeLayout(false);
-            this.groupBox4.ResumeLayout(false);
-            this.groupBox5.ResumeLayout(false);
-            ((System.ComponentModel.ISupportInitialize)(this.grid_fechas)).EndInit();
+            this.profesional.ResumeLayout(false);
+            this.profesional.PerformLayout();
+            this.horario.ResumeLayout(false);
             ((System.ComponentModel.ISupportInitialize)(this.grid_horarios)).EndInit();
+            this.groupBox4.ResumeLayout(false);
+            this.fecha.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.grid_fechas)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -254,19 +254,19 @@
         #endregion
 
         private System.Windows.Forms.GroupBox box_especialidad;
-        private System.Windows.Forms.ComboBox cbox_especialidades;
+        private System.Windows.Forms.ComboBox combo_especialidades;
         private System.Windows.Forms.Button aceptar_especialidad;
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.GroupBox groupBox2;
-        private System.Windows.Forms.ComboBox cbox_profesionales;
+        private System.Windows.Forms.GroupBox profesional;
+        private System.Windows.Forms.ComboBox combo_profesionales;
         private System.Windows.Forms.Button aceptar_profesional;
         private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.GroupBox horario;
         private System.Windows.Forms.Button confirmar_turno;
         private System.Windows.Forms.DataGridView grid_horarios;
         private System.Windows.Forms.GroupBox groupBox4;
         private System.Windows.Forms.Button atras;
-        private System.Windows.Forms.GroupBox groupBox5;
+        private System.Windows.Forms.GroupBox fecha;
         private System.Windows.Forms.Button aceptar_fecha;
         private System.Windows.Forms.DataGridView grid_fechas;
         private System.Windows.Forms.DataGridViewCheckBoxColumn elegir_horario;
--- ClinicaFrba/ClinicaFrba/Pedir Turno/pedir_turno.cs
+++ ClinicaFrba/ClinicaFrba/Pedir Turno/pedir_turno.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -12,9 +13,77 @@ namespace ClinicaFrba.Pedir_Turno
 {
     public partial class pedir_turno : Form
     {
-        public pedir_turno()
+       public DBAccess Access { get; set; }
+       public pedir_turno()
         {
             InitializeComponent();
+            Access = new DBAccess();
+            profesional.Hide();
+            fecha.Hide();
+            horario.Hide();
+            cargarespecialidades();
+        }
+
+        private void cargarespecialidades()
+        {
+            SqlConnection conexion = new SqlConnection(Access.Conexion);
+            conexion.Open();
+            string query = "SELECT Nombre FROM UN_CORTADO.ESPECIALIDADES";
+            SqlCommand cmd = new SqlCommand(query, conexion);
+            SqlDataReader dr = cmd.ExecuteReader();
+            while (dr.Read())
+            {
+                combo_especialidades.Items.Add(dr[0]);
+            }
+        }
+
+        private void aceptar_especialidad_Click(object sender, EventArgs e)
+        {            
+            {
+                profesional.Show();
+                cargarprofesionales();
+            }
+        }
+
+        private void cargarprofesionales()
+        {
+            String query = " SELECT NOMBRE_PROFESIONAL,APELLIDO_PROFESIONAL FROM UN_CORTADO.ProfesionalesYSusEspecialidades WHERE NOMBRE_ESPECIALIDAD=@especialidad";
+            SqlConnection con = new SqlConnection(Access.Conexion);
+            SqlCommand cmd = new SqlCommand(query, con);
+            cmd.Parameters.AddWithValue("@especialidad", combo_especialidades.SelectedItem.ToString());
+            SqlDataReader dr;
+            try
+            {
+                con.Open();
+                dr = cmd.ExecuteReader();
+                while (dr.Read())
+                {
+                    string nombre_completo = dr["APELLIDO_PROFESIONAL"] + "," + dr["NOMBRE_PROFESIONAL"];
+                    combo_profesionales.Items.Add(nombre_completo);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Ocurrió un error","ERROR");
+            }
+        }
+
+        private void aceptar_profesional_Click(object sender, EventArgs e)
+        {
+            fecha.Show();
+            cargarfechas();
+        }
+
+        private void cargarfechas()
+        {
+
+        }
+
+        private void aceptar_fecha_Click(object sender, EventArgs e)
+        {
+            horario.Show();
         }
     }
 }
+
+
